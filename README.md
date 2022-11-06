8f28a6ddf942b98550490ffbdc455165f5867bac
peace317 <102929517+peace317@users.noreply.github.com>
2022-11-06 23:42:04 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# MinTerm
+
+A project for serialport communication of using [electron-react-boilerplate](https://github.com/chentsulin/electron-react-boilerplate).
+
+### Installation
+
+```bash
+npm install
+npm start
+```
---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -it -v "C:\Users\janik\OneDrive\Desktop\technische_Informatik\Semester 6\Laborprojekt\laborprojekt_08\MinTerm:/project" \
+  -w /project electronuserland/builder \
+#  yarn global add electron-builder \
+#  && electron-builder -l
+#  yarn add yarn upgrade \
+#docker exec cont yarn upgrade
+#-v "C:\Users\janik\OneDrive\Desktop\technische_Informatik\Semester 6\Laborprojekt\laborprojekt_08\MinTerm:/scripts" $my_docker_build /bin/bash
+#docker exec yarn upgrade
+#docker exec yarn global add electron-builder
+#docker exec electron-builder -l
