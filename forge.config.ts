45a92388c56650f418db304d08554e4383f0995f
Janik Piepenhagen <janik.piepenhagen@web.de>
2024-08-12 23:54:31 +0200

    Included post install hook to add serialport as native dep to the package

--- forge.config.ts
+++ forge.config.ts
@@ -1,22 +1,29 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
-
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import path from "path";
+import fs from "fs";
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
+import { spawn } from "child_process";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
@@ -25,11 +32,11 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/renderer/index.html',
-            js: './src/renderer/index.tsx',
-            name: 'main_window',
+            html: "./src/renderer/index.html",
+            js: "./src/renderer/index.tsx",
+            name: "main_window",
             preload: {
-              js: './src/main/preload.ts',
+              js: "./src/main/preload.ts",
             },
           },
         ],
@@ -47,6 +54,72 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  hooks: {
+    packageAfterPrune: async (_, buildPath, __, platform) => {
+      const commands = [
+        "install",
+        "--no-package-lock",
+        "--no-save",
+        "serialport",
+      ];
+
+      return new Promise((resolve, reject) => {
+        const oldPckgJson = path.join(buildPath, "package.json");
+        const newPckgJson = path.join(buildPath, "_package.json");
+
+        fs.renameSync(oldPckgJson, newPckgJson);
+
+        const npmInstall = spawn("npm", commands, {
+          cwd: buildPath,
+          stdio: "inherit",
+          shell: true,
+        });
+
+        npmInstall.on("close", (code) => {
+          if (code === 0) {
+            fs.renameSync(newPckgJson, oldPckgJson);
+
+            /**
+             * On windows code signing fails for ARM binaries etc.,
+             * we remove them here
+             */
+            if (platform === "win32") {
+              const problematicPaths = [
+                "android-arm",
+                "android-arm64",
+                "darwin-x64+arm64",
+                "linux-arm",
+                "linux-arm64",
+                "linux-x64",
+              ];
+
+              problematicPaths.forEach((binaryFolder) => {
+                fs.rmSync(
+                  path.join(
+                    buildPath,
+                    "node_modules",
+                    "@serialport",
+                    "bindings-cpp",
+                    "prebuilds",
+                    binaryFolder
+                  ),
+                  { recursive: true, force: true }
+                );
+              });
+            }
+
+            resolve();
+          } else {
+            reject(new Error("process finished with error code " + code));
+          }
+        });
+
+        npmInstall.on("error", (error) => {
+          reject(error);
+        });
+      });
+    },
+  },
 };
 
 export default config;
